6d0bca2f89f52de89d5c45bf22728a7ba43aefe9
leolainen <leolainen2@gmail.com>
2020-04-24 00:35:56 +0200

    Updates

---
+++ precache-manifest.f376cb53aeea0440884f778e49e9a4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42fa0eba85eb234fb08f859f709264b0",
+    "url": "/ATG_test_assignment/index.html"
+  },
+  {
+    "revision": "0c53c63eea2b59ee7d0f",
+    "url": "/ATG_test_assignment/static/css/main.4b4101a8.chunk.css"
+  },
+  {
+    "revision": "cc7e10e9d9dd3a976ecd",
+    "url": "/ATG_test_assignment/static/js/2.c683807f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ATG_test_assignment/static/js/2.c683807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c53c63eea2b59ee7d0f",
+    "url": "/ATG_test_assignment/static/js/main.ce04a1a9.chunk.js"
+  },
+  {
+    "revision": "a7da58be334bf4bc9798",
+    "url": "/ATG_test_assignment/static/js/runtime-main.f82b69dd.js"
+  }
+]);
