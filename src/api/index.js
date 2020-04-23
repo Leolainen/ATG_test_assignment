9f3eb664a29937e9340d2e45fd621de40c66e601
leolainen <leolainen2@gmail.com>
2020-04-24 00:15:29 +0200

    [api] Remove useless code

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,8 @@
 import { PRODUCTS_URL, GAMES_URL } from "constants/api";
 
-// Products
-export const fetchProducts = async () => {
-  return await fetchRace(PRODUCTS_URL);
-};
-
 // expeted gameTypes:   V75, V65, V64, V4
 export const fetchProductByType = async (gameType) => {
   return await fetchRace(PRODUCTS_URL, gameType.toUpperCase());
-};
-
-// Games
-export const fetchGames = async () => {
-  return await fetchRace(GAMES_URL);
 };
 
 export const fetchGameById = async (gameId) => {
