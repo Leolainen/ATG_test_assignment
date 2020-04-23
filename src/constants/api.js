5cd43d42c4f866a512b94d9c9109e5c4161eef70
leolainen <leolainen2@gmail.com>
2020-04-24 00:38:37 +0200

    [constants] Change to secure http

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
 export const PRODUCTS_URL =
   "https://www.atg.se/services/racinginfo/v1/api/products/";
-export const GAMES_URL = "http://www.atg.se/services/racinginfo/v1/api/games/";
+export const GAMES_URL = "https://www.atg.se/services/racinginfo/v1/api/games/";
